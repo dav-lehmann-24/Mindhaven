6ae21534df38762348d3f2418d909517fe8a614a
Hafsa-jabeen <140058033+Hafsa-jabeen@users.noreply.github.com>
2026-03-16 23:44:02 +0100

    database hosted on aiven

--- README.md
+++ README.md
@@ -15,3 +15,19 @@ npm start
 
 ### Generating class diagrams 
   option+D on mac 
+
+## Connecting the local API to an Aiven MySQL database
+1. In the Aiven console download the `ca.pem` certificate for your MySQL service. Save it inside `server/certs` (for example `server/certs/aiven-ca.pem`).
+2. Update `server/.env` with your service credentials: 
+   ```env
+   DB_HOST=<aiven-hostname>
+   DB_USER=<avnadmin>
+   DB_PASSWORD=<your-aiven-password>
+   DB_DATABASE=<database-name>
+   DB_PORT=<service-port>
+   DB_SSL=true
+   DB_SSL_CA_FILE=certs/aiven-ca.pem
+   DB_SSL_REJECT_UNAUTHORIZED=true
+   ```
+   You can also provide the certificate content directly via `DB_SSL_CA_BASE64` if you cannot store files locally.
+3. Start the API from the `server` directory (`node index.js`). The server will now establish an SSL connection to the remote Aiven database while still running locally.
--- server/config/database.js
+++ server/config/database.js
@@ -1,17 +1,65 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
+// const fs = require('fs');
+// const path = require('path');
 
+// require('dotenv').config();
+
+// const connectionConfig = {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     port: process.env.DB_PORT
+// };
+
+// if (process.env.DB_SSL === 'true') {
+//     const sslOptions = {};
+
+//     if (process.env.DB_SSL_CA_FILE) {
+//         const caPath = path.resolve(process.cwd(), process.env.DB_SSL_CA_FILE);
+//         try {
+//             sslOptions.ca = fs.readFileSync(caPath);
+//         } catch (err) {
+//             console.error(`Unable to read CA file at ${caPath}`);
+//             throw err;
+//         }
+//     } else if (process.env.DB_SSL_CA_BASE64) {
+//         sslOptions.ca = Buffer.from(process.env.DB_SSL_CA_BASE64, 'base64');
+//     }
+
+//     sslOptions.rejectUnauthorized = process.env.DB_SSL_REJECT_UNAUTHORIZED !== 'false';
+//     if (Object.keys(sslOptions).length > 0) {
+//         connectionConfig.ssl = sslOptions;
+//     }
+// }
+
+// const connection = mysql.createConnection(connectionConfig);
+
+// connection.connect((err) => {
+//     if (err) throw err;
+//     console.log('Connected to MySQL database');
+// });
+
+// module.exports = connection;
+
+
+const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const connectionConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
-});
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED !== 'false'
+    }
+};
 
+const connection = mysql.createPool(connectionConfig);
 
-connection.connect((err) => {
+connection.query('SELECT 1', (err) => {
     if (err) throw err;
     console.log('Connected to MySQL database');
 });
