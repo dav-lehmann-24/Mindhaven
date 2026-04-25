66b4f59e3494958046d6ec06c557a75bb48f064f
Hafsa-jabeen <140058033+Hafsa-jabeen@users.noreply.github.com>
2026-04-25 12:17:49 +0200

    buddy functionality

---
+++ server/routes/buddyRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { verifyToken } = require('../middleware/authMiddleware');
+const buddyController = require('../controllers/buddyController');
+
+router.post('/', verifyToken, buddyController.connectBuddy);
+router.get('/', verifyToken, buddyController.listBuddies);
+router.get('/pending', verifyToken, buddyController.listPendingRequests);
+router.get('/:id/tasks', verifyToken, buddyController.getBuddyTasks);
+router.post('/:id/tasks', verifyToken, buddyController.addBuddyTask);
+router.patch('/:id/tasks/:taskId', verifyToken, buddyController.toggleBuddyTask);
+router.put('/streak', verifyToken, buddyController.updateStreak);
+router.put('/:id/accept', verifyToken, buddyController.acceptBuddyRequest);
+router.delete('/:id/reject', verifyToken, buddyController.rejectBuddyRequest);
+router.get('/:id', verifyToken, buddyController.getBuddyProfile);
+router.delete('/:id', verifyToken, buddyController.removeBuddy);
+
+module.exports = router;
