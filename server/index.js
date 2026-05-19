4e50e26667c6bc101e6537ab68f11ef985d699ec
dav-lehmann-24 <191463061+dav-lehmann-24@users.noreply.github.com>
2026-05-19 20:59:49 +0200

    Changes in uploadMiddleware and index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 // routes
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(uploadsDir));
 app.use('/api/test', testRoutes);
 app.use('/api/journal', journalRoutes);
 app.use('/api/tags', tagRoutes);
--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,9 +1,11 @@
 const multer = require('multer');
 const path = require('path');
 
+const uploadsDir = path.join(__dirname, '..', 'uploads');
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // folder inside your server root
+    cb(null, uploadsDir); // folder inside your server root
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
